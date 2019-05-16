aff69f6ae0f93fb902305509cbbca910aaba1d13
ZippyMagician <zippymagician1@gmail.com>
2019-05-16 11:39:25 -0400

    Pixel index

---
+++ src/index.js
@@ -0,0 +1,10 @@
+module.exports = {
+  Game: require('./packages/core'),
+  Sprite: require('./packages/sprite'),
+  Texture: require('./packages/texture'),
+  Rectangle: require('./packages/shape/rectangle),
+  Circle: require('./packages/shape/circle'),
+  Text: require('./packages/text'),
+  Sound: require('./packages/sound'),
+  Point: require('./packages/shape/point')
+}
