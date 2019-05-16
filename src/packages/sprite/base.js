657491072222c9cfe881e06a0e2fe35f2e050c9d
ZippyMagician <zippymagician1@gmail.com>
2019-05-16 11:42:48 -0400

    SpriteBase

---
+++ src/packages/sprite/base.js
@@ -0,0 +1,27 @@
+class SpriteBase {
+  constructor() {
+    this.point = new Point();
+  }
+
+  get x() {
+    return this.point.x;
+  }
+
+  set x(v) {
+    return this.point.x = v;
+  }
+
+  get y() {
+    return this.point.y;
+  }
+
+  set y(v) {
+    return this.point.y = v;
+  }
+
+  clone() {
+    return this;
+  }
+}
+
+module.exports = SpriteBase;
