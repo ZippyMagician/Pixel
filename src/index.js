cbc9c7ba337342b5e01f789ee73cbaee60af793b
ZippyMagician <zippymagician1@gmail.com>
2019-05-16 11:39:39 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ module.exports = {
   Game: require('./packages/core'),
   Sprite: require('./packages/sprite'),
   Texture: require('./packages/texture'),
-  Rectangle: require('./packages/shape/rectangle),
+  Rectangle: require('./packages/shape/rectangle'),
   Circle: require('./packages/shape/circle'),
   Text: require('./packages/text'),
   Sound: require('./packages/sound'),
