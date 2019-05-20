429ddb540fa13317e97d98b53f0917a393a0141f
ZippyMagician <zippymagician1@gmail.com>
2019-05-20 18:45:42 -0400

    FillStyle assister class

---
+++ src/packages/graphics/fillstyle.js
@@ -0,0 +1,26 @@
+class FillStyle {
+  constructor() {
+    this.reset();
+  }
+
+  reset() {
+    this.color = 0xFFFFFF;
+    this.alpha = 1;
+  }
+
+  clone() {
+    var f = new FillStyle();
+    f.color = this.color;
+    f.alpha = this.alpha;
+    
+    return f;
+  }
+
+  fill(c) {
+    this.color = c;
+
+    return this.color;
+  }
+}
+
+module.exports = FillStyle;
