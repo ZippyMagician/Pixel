71d00505930260d4048c023fc3d541043cd1c0a1
ZippyMagician <zippymagician1@gmail.com>
2019-06-10 09:35:57 -0400

    Update point.js

--- src/packages/shape/point.js
+++ src/packages/shape/point.js
@@ -1,12 +1,10 @@
-class Point {
-  constructor() {
-    this.x = 0;
-    this.y = 0;
+export default class Point {
+  constructor(x = 0, y = 0) {
+    this.x = x;
+    this.y = y;
   }
 
   clone() {
-    return this;
+    return new Point(this.x, this.y);
   }
 }
-
-module.exports = Point;
