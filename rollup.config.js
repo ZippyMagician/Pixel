dc2afc89f23ba9e167774946f7f766cab7bb32d3
ZippyMagician <zippymagician1@gmail.com>
2019-06-10 13:39:35 -0400

    rollup-plugin-uglify plugin

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,19 @@
-module.exports = {
-  input: 'src/index.js',
-  output: {
-    file: 'dist/Pixel.js',
-    format: 'cjs'
+var { uglify } = require('rollup-plugin-uglify');
+
+module.exports = [
+  {
+    input: 'src/index.js',
+    output: {
+      file: 'dist/Pixel.js',
+      format: 'cjs'
+    }
+  },
+  {
+    input: 'src/index.js',
+    output: {
+      file: 'dist/Pixel.min.js',
+      format: 'cjs'
+    },
+    plugins: [uglify()]
   }
-};
+];
