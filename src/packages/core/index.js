9d3c2ab457ecdb34fe5ed1f2e667936416990f03
ZippyMagician <zippymagician1@gmail.com>
2019-06-10 09:23:16 -0400

    Fix

--- src/packages/core/index.js
+++ src/packages/core/index.js
@@ -1,32 +1,4 @@
-class Point {
-  constructor(x = 0, y = 0) {
-    this.x = x;
-    this.y = y;
-  }
-
-  clone() {
-    return new Point(this.x, this.y);
-  }
-}
-
-function chooseRandom(array) {
-  return array[Math.floor(Math.random() * (array.length - 1))];
-}
-
-var _pixel_keys = {
-  1: "1", 2: "2", 3: "3", 4: "4", 5: "5", 6: "6", 7: "7", 8: "8", 9: "9", 0: "0",
-  TAB: "Tab", SHIFT: "Shift", COMMA: ",", PERIOD: ".", BACKSLASH: "/", TILDE: "~", EXCLAMATION: "!", ATSIGN: "@", HASHTAG: "#", DOLLARSIGN: "$", PERCENT: "%", DASH: "-", UNDERSCORE: "_", PLUS: "+", QUESTIONMARK: "?",
-  A: "a", B: "b", C: "c", D: "d", E: "e", F: "f", G: "g", H: "h", I: "i", J: "j", K: "k", L: "l", M: "m", N: "n", O: "o", P: "p", Q: "q", R: "r", S: "s", T: "t", U: "u", V: "v", W: "w", X: "x", Y: "y", Z: "z",
-  UP: "ArrowUp", DOWN: "ArrowDown", LEFT: "ArrowLeft", RIGHT: "ArrowRight",
-  down: {
-    1: false,2: false,3: false,4: false,5: false,6: false,7: false,8: false,9: false,0: false,
-    TAB: false,SHIFT: false,COMMA: false,PERIOD: false,BACKSLASH: false,TILDE: false,EXCLAMATION: false,ATSIGN: false,HASHTAG: false,DOLLARSIGN: false,PERCENT: false,DASH: false,UNDERSCORE: false,PLUS: false,QUESTIONMARK: false,
-    A: false,B: false,C: false,D: false,E: false,F: false,G: false,H: false,I: false,J: false,K: false,L: false,M: false,N: false,O: false,P: false,Q: false,R: false,S: false,T: false,U: false,V: false,W: false,X: false,Y: false,Z: false,
-    UP: false,DOWN: false,LEFT: false,RIGHT: false
-  }
-};
-
-class Stage {
+export class Stage {
   constructor(options) {
     var self = this;
     var canvas = document.createElement("canvas");
