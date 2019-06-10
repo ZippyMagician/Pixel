3145aa5ba40d22738292fb01dc6394af69e75263
ZippyMagician <zippymagician1@gmail.com>
2019-06-10 09:31:23 -0400

    Update base.js

--- src/packages/sprite/base.js
+++ src/packages/sprite/base.js
@@ -1,9 +1,20 @@
-var Point = require('../shape/point');
+import { Point } from '../shape';
 
-class SpriteBase {
-  constructor() {
+export class SpriteBase {
+  constructor(noscale = false) {
     this.point = new Point();
     this.anchor = new Point();
+    this.opacity = 1.0;
+    this.deg = 0;
+    this.scale = 1;
+    this.noscale = noscale;
+    this.flipX = false;
+    this.flipY = false;
+  }
+
+  setSize(w, h) {
+    this.width = w;
+    this.height = h;
   }
 
   get x() {
@@ -11,7 +22,7 @@ class SpriteBase {
   }
 
   set x(v) {
-    return this.point.x = v;
+    return (this.point.x = v);
   }
 
   get y() {
@@ -19,17 +30,35 @@ class SpriteBase {
   }
 
   set y(v) {
-    return this.point.y = v;
+    return (this.point.y = v);
   }
-  
+
   setAnchor(x, y) {
     this.anchor.x = x;
     this.anchor.y = y;
   }
 
-  clone() {
-    return this;
+  copy(sprite) {
+    this.point = sprite.point;
+    this.deg = sprite.deg;
+    this.anchor = sprite.anchor;
+    this.flipX = sprite.flipX;
+    this.flipY = sprite.flipY;
+    this.opacity = sprite.opacity;
+    this.scale = sprite.scale;
+  }
+
+  spin(num) {
+    this.deg = num * 360;
+  }
+
+  settings(ctx) {
+    if (!this.noscale) ctx.globalAlpha = this.opacity;
+    ctx.scale(this.scale, this.scale);
+  }
+
+  reset(ctx) {
+    ctx.globalAlpha = 1.0;
+    if (!this.noscale) ctx.scale(1 / this.scale, 1 / this.scale);
   }
 }
-
-module.exports = SpriteBase;
