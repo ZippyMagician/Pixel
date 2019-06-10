5a921d86c68858f749a16dd96fc9a0bbb3fae18b
ZippyMagician <zippymagician1@gmail.com>
2019-06-10 09:26:31 -0400

    Update index.js

--- src/packages/sprite/index.js
+++ src/packages/sprite/index.js
@@ -1,32 +1,82 @@
-var SpriteBase = require('./base');
+import SpriteBase from './base';
+import AnimatedSprite from './animation';
+import SpriteSheet from './sheet';
 
-class Sprite extends SpriteBase {
+export class Sprite extends SpriteBase {
   constructor(texture) {
     super();
-    var self = this;
-    this.texture = texture
+    this.texture = texture;
   }
 
   render(canvas) {
     if (this.texture.renderable) {
-      if (this.deg > 0) this.rotation_render(canvas)
-      else canvas.drawImage(this.texture.image, this.x, this.y);
+      this.settings(canvas);
+      if (this.deg > 0 || this.flipX || this.flipY)
+        this.rotation_render(canvas);
+      else
+        canvas.drawImage(
+          this.texture.image,
+          this.x / this.scale - this.texture.image.width * this.anchor.x,
+          this.y / this.scale - this.texture.image.height * this.anchor.y
+        );
+      this.reset(canvas);
     }
   }
 
   rotation_render(ctx) {
     var image = this.texture.image;
-    var width = ((ctx.canvas.width * this.anchor.x));
-    var height = ((ctx.canvas.height * this.anchor.y));
-    var x = this.x - width - (image.width * this.anchor.x);
-    var y = this.y - height - (image.height * this.anchor.y);
 
     ctx.save();
-    ctx.translate(width - x, height - y);
-    ctx.rotate(Math.PI / 180 * this.deg);
-    ctx.drawImage(image, x, y);
+    ctx.translate(this.x / this.scale, this.y / this.scale);
+    if (this.flipX) ctx.scale(-1, 1);
+    if (this.flipY) ctx.scale(1, -1);
+    ctx.fillStyle = "#000000";
+    ctx.rotate((Math.PI / 180) * this.deg);
+    ctx.drawImage(
+      image,
+      -image.width * this.anchor.x,
+      -image.height * this.anchor.y
+    );
     ctx.restore();
+  }
+
+  checkCollisions(otherobj) { // Ported from https://www.w3schools.com/graphics/game_obstacles.asp
+    let bias = 0;
+    var myleft = this.x / this.scale - this.texture.image.width * this.anchor.x;
+    var myright = myleft + (this.texture.image.width);
+    var mytop = this.y / this.scale - this.texture.image.height * this.anchor.y;
+    var mybottom = mytop + (this.texture.image.height);
+    var otherleft = otherobj.x / otherobj.scale - (otherobj.width || otherobj.texture.image.width) * otherobj.anchor.x;
+    var otherright = otherleft + (otherobj.width || otherobj.texture.image.width) / otherobj.scale;
+    var othertop = otherobj.y / otherobj.scale - (otherobj.height || otherobj.texture.image.height) * otherobj.anchor.y;
+    var otherbottom = othertop + (otherobj.height || otherobj.texture.image.height) / otherobj.scale;
+    //console.log(this.anchor.x, this.anchor.y);
+    //console.log(myleft, otherleft, myright, otherright, mytop, othertop, mybottom, otherbottom);
+    var crash = true;
+    var left = false;
+    var right = false;
+    var top = false;
+    var bottom = false;
+
+    if ((mybottom < othertop) ||
+        (mytop > otherbottom) ||
+        (myright < otherleft) ||
+        (myleft > otherright)) {
+      crash = false;
+    }
+    if (crash) {
+      if (mybottom - bias > otherbottom && mytop + bias < otherbottom) bottom = true;
+      if (mytop + bias < othertop && mybottom - bias > othertop) top = true;
+      if (myleft + bias < otherleft && myright - bias > otherleft) left = true;
+      if (myright - bias > otherright && myleft + bias < otherright) right = true;
+    }
+    return {body: crash, left: left, right: right, top: top, bottom: bottom};
+  }
+
+  clone() {
+    return new Sprite(this.texture);
   }
 }
 
-module.exports = Sprite;
+export AnimatedSprite;
+export SpriteSheet;
