bd09c472e4cbcf6a7cc7fe6b08c2f46eeb8bd4ed
ZippyMagician <zippymagician1@gmail.com>
2019-06-11 08:34:54 -0400

    Update test-docs.js

--- build/test-docs.js
+++ build/test-docs.js
@@ -1,11 +1,5 @@
 var fs = require('fs');
 
-fs.readdir('../docs', (err, files) => {
-  files.forEach(file => {
-    console.log(file);
-  });
-});
-
 fs.readdir('./docs', (err, files) => {
   files.forEach(file => {
     console.log(file);
