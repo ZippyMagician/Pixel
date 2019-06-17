d189f49e49f5db7d30ee79e7e83682ced23632b1
ZippyMagician <zippymagician1@gmail.com>
2019-06-17 10:45:06 -0400

    Create push.sh

---
+++ build/push.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+if [[ $TRAVIS_BRANCH != master ]]; then
+  msg "Not pushing updates to branch $TRAVIS_BRANCH"
+  return 0
+fi
+git pull https://github.com/ZippyMagician/Pixel-Docs
+git config --global user.email "$GH_EMAIL"
+git config --global user.name "$GH_USERNAME"
+git config --global push.default current
+git stash
+git checkout ${TRAVIS_BRANCH}
+git stash pop
+grunt release
+git push https://${GH_TOKEN}@github.com/ZippyMagician/Pixel-Docs.git
