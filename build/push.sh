98820d207d0be967ddbd6f8d21c60acaa9cdf76c
ZippyMagician <zippymagician1@gmail.com>
2019-06-17 10:53:33 -0400

    Attempt to fix build details

--- build/push.sh
+++ build/push.sh
@@ -1,15 +1,19 @@
 #!/bin/sh
 
+git config --global user.email "$GH_EMAIL"
+git config --global user.name "$GH_USERNAME"
+git config --global push.default current
+
 if [[ $TRAVIS_BRANCH != master ]]; then
   msg "Not pushing updates to branch $TRAVIS_BRANCH"
   return 0
 fi
+
 git clone https://github.com/ZippyMagician/Pixel-Docs
-git config --global user.email "$GH_EMAIL"
-git config --global user.name "$GH_USERNAME"
-git config --global push.default current
+git pull https://github.com/ZippyMagician/Pixel-Docs
+
 git stash
 git checkout ${TRAVIS_BRANCH}
 git stash pop
-grunt release
+
 git push https://${GH_TOKEN}@github.com/ZippyMagician/Pixel-Docs.git
