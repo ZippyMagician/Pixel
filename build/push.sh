9d65f603acd5fff2c6cc736c87a51c34852412c8
ZippyMagician <zippymagician1@gmail.com>
2019-06-17 10:49:31 -0400

    `pull` --> `clone`

--- build/push.sh
+++ build/push.sh
@@ -4,7 +4,7 @@ if [[ $TRAVIS_BRANCH != master ]]; then
   msg "Not pushing updates to branch $TRAVIS_BRANCH"
   return 0
 fi
-git pull https://github.com/ZippyMagician/Pixel-Docs
+git clone https://github.com/ZippyMagician/Pixel-Docs
 git config --global user.email "$GH_EMAIL"
 git config --global user.name "$GH_USERNAME"
 git config --global push.default current
